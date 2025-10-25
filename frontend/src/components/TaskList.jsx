4e5366a838de01953559cb0286a383b840da1eed
Wilson Tochi Uduma <tochiwilson10@gmail.com>
2025-10-25 21:31:21 +0200

    add task description and priority display in TaskList component

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -9,6 +9,8 @@ const TaskList = ({ tasks, onDelete }) => {
         {tasks.map((task) => (
           <li key={task._id}>
             <span>#{task.taskNumber} - {task.title}</span>
+            <p>{task.description}</p>
+            <span>{task.priority}</span>
             <button onClick={() => onDelete(task._id)}>Delete</button>
           </li>
         ))}
