b6d473ddc596b38aa85a7d97ccc23006d9914906
Wilson Tochi Uduma <tochiwilson10@gmail.com>
2025-10-25 21:32:50 +0200

    implement priority classification model training and evaluation

---
+++ backend/train_model.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+import joblib
+
+INPUT_FILE = "./dataset/cleaned_tasks.csv"
+MODEL_FILENAME = "./model/priority_classifier_model.pkl"
+
+try:
+    df = pd.read_csv(INPUT_FILE)
+    print(f"Data successfully loaded from: {INPUT_FILE} (Size: {len(df)})")
+except FileNotFoundError:
+    print(f"Error: {INPUT_FILE} not found. Please make sure you have run clean_data.py.")
+    exit()
+
+# features and labels
+X = df["task"]  # input: task + descriptions
+y = df["priority_label"]  # outpuy: (High/Medium/Low)
+
+# split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+# pipeline: vectorizer for text + classifier for priority
+model_pipeline = Pipeline(
+    [("vectorizer", TfidfVectorizer()), ("classifier", MultinomialNB())]
+)
+
+# train model
+print("Starting model training...")
+model_pipeline.fit(X_train, y_train)
+print("Model successfully trained!")
+
+# model evaluation
+y_pred = model_pipeline.predict(X_test)
+
+# classification report
+print("\n" + "=" * 50)
+print("MODEL EVALUATION REPORT ON TEST DATA")
+print("=" * 50)
+print(classification_report(y_test, y_pred))
+
+# save model
+joblib.dump(model_pipeline, MODEL_FILENAME)
+print("=" * 50)
+print(f"Model successfully saved as '{MODEL_FILENAME}'.")
