5ac4f2e4deac4eb0ee95f3ef7feabef593e4256d
Wilson Tochi Uduma <tochiwilson10@gmail.com>
2025-10-25 21:33:06 +0200

    refactor task models to use Pydantic Field and define priority type

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,14 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional, Literal
+
+PriorityType = Literal['low', 'medium', 'high']
 
 class Task(BaseModel):
-    title: str
-    description: str
-    priority: str | None = None
+    title: str = Field(..., description="Title of the task")
+    description: str = Field(..., description="Description of the task")
+    priority: Optional[PriorityType] = None
 
 class UpdateTask(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    priority: Optional[str] = None
+    priority: Optional[PriorityType] = None
